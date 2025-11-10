5fb8965eaee47eb55577f1383b84b7bcf7702d0a
Pazl <130466427+Pazl27@users.noreply.github.com>
2025-11-10 15:09:15 +0100

    read data

--- src/main/java/de/hft/stuttgart/App.java
+++ src/main/java/de/hft/stuttgart/App.java
@@ -1,13 +1,17 @@
 package de.hft.stuttgart;
 
-/**
- * Hello world!
- *
- */
+import java.util.Scanner;
+
 public class App
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws Exception
     {
-        System.out.println( "Hello World!" );
+        final Scanner scanner = new Scanner(App.class.getClassLoader().getResourceAsStream("diabetes.csv"));
+        while (scanner.hasNextLine()) {
+            System.out.println(scanner.nextLine());
+        }
+
+        scanner.close();
+
     }
 }
